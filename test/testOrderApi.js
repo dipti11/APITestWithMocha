8b78b3bf622e6eb331179d77d536daa7e721661b
patil.dipti11 <patil.dipti11@gmail.com>
2021-08-12 20:10:08 +0530

    added async await

--- test/testOrderApi.js
+++ test/testOrderApi.js
@@ -15,7 +15,8 @@ const testHelpers = require('./testHelpers');
 describe('POST order api', function() {
 
   it('should be able to create order successfully without order at time', async () => {
-    const response = await testData.getRequestURL().post('/v1/orders')
+    const response = await testData.getRequestURL()
+      .post('/v1/orders')
       .set('Content-Type', 'application/json')
       .send(testHelpers.orderCreatePayload())
       .timeout(10000);
@@ -35,7 +36,8 @@ describe('POST order api', function() {
 
     createOrderPayload.orderAt = orderAt;
 
-    const response = await testData.getRequestURL().post('/v1/orders')
+    const response = await testData.getRequestURL()
+      .post('/v1/orders')
       .set('Content-Type', 'application/json')
       .send(createOrderPayload)
       .timeout(10000);
@@ -45,7 +47,7 @@ describe('POST order api', function() {
     verifyCreateOrderResponse(response, true);
   });
 
-  it('should fail to create order with only one stop', function(done) {
+  it('should fail to create order with only one stop', async () => {
     const randomLocation = require('random-location');
     const createOrderPayload = {};
     createOrderPayload.stops = [];
@@ -53,22 +55,16 @@ describe('POST order api', function() {
 
     createOrderPayload.stops.push(testHelpers.getLatitudeLongitudeAsPerPayload(randomStop1));
 
-    testData.getRequestURL().post('/v1/orders')
+    const response = await testData.getRequestURL()
+      .post('/v1/orders')
       .set('Content-Type', 'application/json')
-      .send(createOrderPayload)
-      .then(response => {
-        console.log("response for one stop in payload---- ");
-        console.log(response.body);
-        verifyErrorResponse(response, StatusCodes.BAD_REQUEST, 'error in field(s): stops');
-        done();
-      })
-      .catch(error => {
-        console.log('Error - ' + error.message);
-        throw (error);
-      });
+      .send(createOrderPayload);
+    console.log("response for one stop in payload---- ");
+    console.log(response.body);
+    verifyErrorResponse(response, StatusCodes.BAD_REQUEST, 'error in field(s): stops');
   });
 
-  it('should fail to create order with incorrect co-ordinates', function(done) {
+  it('should fail to create order with incorrect co-ordinates', async () => {
 
     const createOrderPayload = {};
     createOrderPayload.stops = [];
@@ -78,35 +74,25 @@ describe('POST order api', function() {
     //adding invalid co-ordinates
     createOrderPayload.stops.push(testHelpers.getLatitudeLongitudeAsPerPayload(testData.incorrectCoOrdinates));
 
-    testData.getRequestURL().post('/v1/orders')
+    const response = await testData.getRequestURL()
+      .post('/v1/orders')
       .set('Content-Type', 'application/json')
-      .send(createOrderPayload)
-      .then(response => {
-        console.log("response for incorrect location ---- ");
-        console.log(response.body);
-        verifyErrorResponse(response, StatusCodes.SERVICE_UNAVAILABLE, 'Service Unavailable');
-        done();
-      })
-      .catch(error => {
-        console.log('Error - ' + error.message);
-        throw (error);
-      });
+      .send(createOrderPayload);
+
+    console.log("response for incorrect location ---- ");
+    console.log(response.body);
+    verifyErrorResponse(response, StatusCodes.SERVICE_UNAVAILABLE, 'Service Unavailable');
   });
 
-  it('should fail to create order with missing payload', function(done) {
-    testData.getRequestURL().post('/v1/orders')
+  it('should fail to create order with missing payload', async () => {
+    const response = await testData.getRequestURL()
+      .post('/v1/orders')
       .set('Content-Type', 'application/json')
-      .send('')
-      .then(response => {
-        console.log("response for missing payload --- ");
-        console.log(response.body);
-        verifyErrorResponse(response, StatusCodes.BAD_REQUEST, '');
-        done();
-      })
-      .catch(error => {
-        console.log('create order post api error - ' + error.message);
-        throw (error);
-      });
+      .send('');
+
+    console.log("response for missing payload --- ");
+    console.log(response.body);
+    verifyErrorResponse(response, StatusCodes.BAD_REQUEST, '');
   });
 });
 
@@ -124,7 +110,8 @@ describe('GET order api', function() {
 
     console.log("order ID ---- " + orderId);
     //fetching the order with assigning state
-    testData.getRequestURL().get('/v1/orders/' + orderId)
+    testData.getRequestURL()
+      .get('/v1/orders/' + orderId)
       .then(response => {
         console.log("fetch order response for orderId ----" + orderId);
         console.log(response.body);
@@ -136,19 +123,15 @@ describe('GET order api', function() {
       });
   });
 
-  it('should fail to fetch order for incorrect order id', function(done) {
+  it('should fail to fetch order for incorrect order id', async () => {
 
-    testData.getRequestURL().get('/v1/orders/' + testData.incorrectOrderId)
-      .then(response => {
-        console.log("fetch order response for incorrect ID ---- ");
-        console.log(response.body);
-        verifyErrorResponse(response, StatusCodes.NOT_FOUND, "ORDER_NOT_FOUND");
-        done();
-      })
-      .catch(err => {
-        console.log('fetch order get api error: ' + err.message);
-        throw (error);
-      });
+    const response = await testData.getRequestURL()
+      .get('/v1/orders/' + testData.incorrectOrderId);
+
+    console.log("fetch order response for incorrect ID ---- ");
+    console.log(response.body);
+    verifyErrorResponse(response, StatusCodes.NOT_FOUND, "ORDER_NOT_FOUND");
+
   });
 });
 
@@ -166,21 +149,24 @@ describe('PUT api for take order', function() {
 
     console.log("order ID ---- " + orderId);
     //passing the above order id to take put api
-    testData.getRequestURL().put('/v1/orders/' + orderId + '/take')
+    testData.getRequestURL()
+      .put('/v1/orders/' + orderId + '/take')
       .then(response => {
         console.log("take order response for order id ---- " + orderId);
         console.log(response.body);
         verifyUpdateOrderResponse(response, "ONGOING", StatusCodes.OK, orderId);
 
         //here we are verifying the fetch ongoing order get api
-        testData.getRequestURL().get('/v1/orders/' + orderId)
+        testData.getRequestURL()
+          .get('/v1/orders/' + orderId)
           .then(getResponse => {
             console.log("fetch ongoing order response for orderId ---- " + orderId);
             console.log(getResponse.body);
             verifyFetchOrderResponse(getResponse, "ONGOING", orderId);
 
             // here we are verifying fail error to take order that is in ongoing status
-            testData.getRequestURL().put('/v1/orders/' + orderId + '/take')
+            testData.getRequestURL()
+              .put('/v1/orders/' + orderId + '/take')
               .then(takeResponse => {
                 console.log("take order error response for ongoing order---- " + orderId);
                 console.log(takeResponse.body);
@@ -202,19 +188,15 @@ describe('PUT api for take order', function() {
       });
   });
 
-  it('should fail to take order for incorrect order id', function(done) {
+  it('should fail to take order for incorrect order id', async () => {
 
-    testData.getRequestURL().put('/v1/orders/' + testData.incorrectOrderId + '/take')
-      .then(response => {
-        console.log("take order error response for incorrect order id---- " + testData.incorrectOrderId);
-        console.log(response.body);
-        verifyErrorResponse(response, StatusCodes.NOT_FOUND, "ORDER_NOT_FOUND");
-        done();
-      })
-      .catch(err => {
-        console.log('take order put api error: ' + err.message);
-        throw (error);
-      });
+    const response = await testData.getRequestURL()
+      .put('/v1/orders/' + testData.incorrectOrderId + '/take');
+
+    console.log("take order error response for incorrect order id---- " + testData.incorrectOrderId);
+    console.log(response.body);
+    verifyErrorResponse(response, StatusCodes.NOT_FOUND, "ORDER_NOT_FOUND");
+
   });
 });
 
@@ -231,26 +213,30 @@ describe('PUT api for complete order', function() {
     const orderId = response.body.id;
     console.log("order ID to complete the order---- " + orderId);
     //passing the above order id to take put api (assigning -> ongoing)
-    testData.getRequestURL().put('/v1/orders/' + orderId + '/take')
+    testData.getRequestURL()
+      .put('/v1/orders/' + orderId + '/take')
       .then(response => {
         console.log("take order response for completing the order id---- " + orderId);
         console.log(response.body);
         //here we are updating status from ongoing -> complete
-        testData.getRequestURL().put('/v1/orders/' + orderId + '/complete')
+        testData.getRequestURL()
+          .put('/v1/orders/' + orderId + '/complete')
           .then(putResponse => {
             console.log("complete order response for order id---- " + orderId);
             console.log(putResponse.body);
             verifyUpdateOrderResponse(putResponse, "COMPLETED", StatusCodes.OK, orderId);
 
             //here we are verifying fetch order for completed status (get api)
-            testData.getRequestURL().get('/v1/orders/' + orderId)
+            testData.getRequestURL()
+              .get('/v1/orders/' + orderId)
               .then(getResponse => {
                 console.log("fetch completed order response for id---- " + orderId);
                 console.log(getResponse.body);
                 verifyFetchOrderResponse(getResponse, "COMPLETED", orderId);
 
                 // here we are verifying fail error to take order that is in completed status
-                testData.getRequestURL().put('/v1/orders/' + orderId + '/take')
+                testData.getRequestURL()
+                  .put('/v1/orders/' + orderId + '/take')
                   .then(takeResponse => {
                     console.log("take order error response for completed order---- " + orderId);
                     console.log(takeResponse.body);
@@ -277,19 +263,15 @@ describe('PUT api for complete order', function() {
       });
   });
 
-  it('should fail to complete order for incorrect order id', function(done) {
+  it('should fail to complete order for incorrect order id', async () => {
 
-    testData.getRequestURL().put('/v1/orders/' + testData.incorrectOrderId + '/complete')
-      .then(response => {
-        console.log("complete order error response for incorrect order id---- " + testData.incorrectOrderId);
-        console.log(response.body);
-        verifyErrorResponse(response, StatusCodes.NOT_FOUND, "ORDER_NOT_FOUND");
-        done();
-      })
-      .catch(err => {
-        console.log('complete order put api error: ' + err.message);
-        throw (error);
-      });
+    const response = await testData.getRequestURL()
+      .put('/v1/orders/' + testData.incorrectOrderId + '/complete');
+
+    console.log("complete order error response for incorrect order id---- " + testData.incorrectOrderId);
+    console.log(response.body);
+    verifyErrorResponse(response, StatusCodes.NOT_FOUND, "ORDER_NOT_FOUND");
+
   });
 
   it('should fail to complete order for invalid status order', async () => {
@@ -303,7 +285,8 @@ describe('PUT api for complete order', function() {
     console.log("order ID to complete the order---- " + orderId);
 
     // here we are verifying the fail to complete order error that is in assigning status
-    testData.getRequestURL().put('/v1/orders/' + orderId + '/complete')
+    testData.getRequestURL()
+      .put('/v1/orders/' + orderId + '/complete')
       .then(takeResponse => {
         console.log("complete order error response for completed order---- " + orderId);
         console.log(takeResponse.body);
@@ -328,21 +311,24 @@ describe('PUT api for cancel order', function() {
     const orderId = response.body.id;
     console.log("order ID to cancel the order---- " + orderId);
     //here we are passing the above order Id to cancel api to test assigning -> cancelled
-    testData.getRequestURL().put('/v1/orders/' + orderId + '/cancel')
+    testData.getRequestURL()
+      .put('/v1/orders/' + orderId + '/cancel')
       .then(cancelResponse => {
         console.log("cancel order response for order id---- " + orderId);
         console.log(cancelResponse.body);
         verifyUpdateOrderResponse(cancelResponse, "CANCELLED", StatusCodes.OK, orderId);
 
         //here we are verifying fetch cancelled order api response
-        testData.getRequestURL().get('/v1/orders/' + orderId)
+        testData.getRequestURL()
+          .get('/v1/orders/' + orderId)
           .then(getResponse => {
             console.log("fetch cancelled order response for id---- " + orderId);
             console.log(getResponse.body);
             verifyFetchOrderResponse(getResponse, "CANCELLED", orderId);
 
             // here we are verifying the fail to take order response that is in cancelled status
-            testData.getRequestURL().put('/v1/orders/' + orderId + '/take')
+            testData.getRequestURL()
+              .put('/v1/orders/' + orderId + '/take')
               .then(takeResponse => {
                 console.log("take order error response for cancelled order---- " + orderId);
                 console.log(takeResponse.body);
@@ -374,12 +360,14 @@ describe('PUT api for cancel order', function() {
     const orderId = response.body.id;
     console.log("order ID to cancel the order---- " + orderId);
     //here we are passing the above order Id to put api for updating status from assigning -> ongoing
-    testData.getRequestURL().put('/v1/orders/' + orderId + '/take')
+    testData.getRequestURL()
+      .put('/v1/orders/' + orderId + '/take')
       .then(response => {
         console.log("take order response for cancelling the order id---- " + orderId);
         console.log(response.body);
         //here we are verifying the put api for cancelling the ongoing order (ongoing -> cancelled)
-        testData.getRequestURL().put('/v1/orders/' + orderId + '/cancel')
+        testData.getRequestURL()
+          .put('/v1/orders/' + orderId + '/cancel')
           .then(cancelResponse => {
             console.log("cancel order response for order id---- " + orderId);
             console.log(cancelResponse.body);
@@ -407,12 +395,14 @@ describe('PUT api for cancel order', function() {
     const orderId = response.body.id;
     console.log("order ID to cancel the order---- " + orderId);
     //here we are passing the above order Id to put api for updating status from assigning -> ongoing
-    testData.getRequestURL().put('/v1/orders/' + orderId + '/take')
+    testData.getRequestURL()
+      .put('/v1/orders/' + orderId + '/take')
       .then(response => {
         console.log("take order response for cancelling the order id---- " + orderId);
         console.log(response.body);
         //here we are updating status from ongoing -> completed
-        testData.getRequestURL().put('/v1/orders/' + orderId + '/complete')
+        testData.getRequestURL().
+        put('/v1/orders/' + orderId + '/complete')
           .then(putResponse => {
             console.log("complete order response for order id---- " + orderId);
             console.log(putResponse.body);
@@ -440,18 +430,14 @@ describe('PUT api for cancel order', function() {
       });
   });
 
-  it('should fail to cancel order for incorrect order id', function(done) {
+  it('should fail to cancel order for incorrect order id', async () => {
 
-    testData.getRequestURL().put('/v1/orders/' + testData.incorrectOrderId + '/cancel')
-      .then(response => {
-        console.log("cancel order error response for incorrect order id---- " + testData.incorrectOrderId);
-        console.log(response.body);
-        verifyErrorResponse(response, StatusCodes.NOT_FOUND, "ORDER_NOT_FOUND");
-        done();
-      })
-      .catch(err => {
-        console.log('cancel order put api error: ' + err.message);
-        throw (error);
-      });
+    const response = await testData.getRequestURL()
+      .put('/v1/orders/' + testData.incorrectOrderId + '/cancel');
+
+    console.log("cancel order error response for incorrect order id---- " + testData.incorrectOrderId);
+    console.log(response.body);
+    verifyErrorResponse(response, StatusCodes.NOT_FOUND, "ORDER_NOT_FOUND");
+
   });
 });
